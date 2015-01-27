0f53c87449b5b154a37af8951469a3260d1f727c
648772021@qq.com <648772021@qq.com>
2015-01-27 16:35:00 +0800

    add function in User.php

--- easyell/model/User.php
+++ easyell/model/User.php
@@ -3,14 +3,11 @@
 class User extends Model{
 	public function __construct() {
 		$this->load('tools/SqlOp.php');
+		$this->load('Group_User.php');
 	}
-	public function selectUserWithSearchKeyAndValue($key, $value) {
-		$this->sqlop->connectTo();
-		$result = $this->sqlop->selectItem("User",$key,$value);
-		$this->sqlop->close();
-		return $result;
-	}
-	
+
+//Public Method
+
 	public function insertUser($id,$account,$username,$password,$avatar,$email,$phone) {
 		$valueArray = array($id, $account, $username, $password, $avatar, $email, $phone);
 		$this->sqlop->connectTo();
@@ -19,19 +16,52 @@ class User extends Model{
 		return $result;
 	}
 
-	public function deleteUserWithKeyAndValue($key, $value) {
-		$this->sqlop->connectTo();
-		$result = $this->sqlop->deleteItem("User",$key, $value);
-		$this->sqlop->close();
-		return $result;
-	}
-	
 	public function updateUser($setKeys, $setValues, $searchKey, $searchValue) {
 		$this->sqlop->connectTo();
 		$result = $this->sqlop->updateUser("User", $setKeys, $setValues, $searchKey, $searchValue);
 		$this->sqlop-close();
 		return $result;
 	}	
+
+	public function selectUserWithId($id) {
+		return $this->selectUserWithSearchKeyAndValue('id', $id);
+	}
+
+	public function selectUserWithAcount($account) {
+		return $this->selectUserWithSearchKeyAndValue('account', $account);
+	}
+
+	public function selectUserWithUsername($username) {
+		return $this->selectUserWithSearchKeyAndValue('username', $username);
+	}
+
+	public function deleteUserWithId($id) {
+		return ($this->deleteUserWithKeyAndValue('id', $id) && $this->group_user->deleteGroup_UserWithUserId($id));
+	}
+
+	public function deleteUserWithAccount($account) {
+		return $this->deleteUserWithKeyAndValue('account', $account);
+	}
+
+	public function deleteUserWithUsername($username) {
+		return $this->deleteUserWithKeyAndValue('username', $username);
+	}
+
+//Private Method
+
+	private function selectUserWithSearchKeyAndValue($key, $value) {
+		$this->sqlop->connectTo();
+		$result = $this->sqlop->selectItem("User",$key,$value);
+		$this->sqlop->close();
+		return $result;
+	}
+
+	private function deleteUserWithKeyAndValue($key, $value) {
+		$this->sqlop->connectTo();
+		$result = $this->sqlop->deleteItem("User",$key, $value);
+		$this->sqlop->close();
+		return $result;
+	}
 }
 
 ?>
