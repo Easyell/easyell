75e857b41f9f9c33362c1250e4a33209a896f361
zhou-yg <zhou.y.g890@gmail.com>
2015-01-29 20:36:35 +0800

    修复语法问题

--- easyell/model/User.php
+++ easyell/model/User.php
@@ -1,59 +1,60 @@
 <?php
-class User extends Model{
-		
-	$id;
-	$account;
-	$username;
-	$password;
-	$avatar;
-	$email;
-	$phone;
+class User extends Model {
+
+	private $id;
+	private $account;
+	private $username;
+	private $password;
+	private $avatar;
+	private $email;
+	private $phone;
 	function __construct() {
-		$this->load('tools/SqlOp.php');
+		$this -> load('tools/SqlOp.php');
 		//$this->load('Group_User.php');
 	}
 
-//Public Method
-	public function insertUser($id,$account,$username,$password,$avatar,$email,$phone) {
+	//Public Method
+	public function insertUser($id, $account, $username, $password, $avatar, $email, $phone) {
 		$valueArray = array($id, $account, $username, $password, $avatar, $email, $phone);
-		$this->SqlOp->connectTo();
-		$result = $this->SqlOp->insertTo("User", $valueArray);
-		$this->SqlOp->close();
+		$this -> SqlOp -> connectTo();
+		$result = $this -> SqlOp -> insertTo("User", $valueArray);
+		$this -> SqlOp -> close();
 		return $result;
 	}
 
 	public function updateUser($setKeys, $setValues, $searchKey, $searchValue) {
-		$this->SqlOp->connectTo();
-		$result = $this->SqlOp->updateItem("User", $setKeys, $setValues, $searchKey, $searchValue);
-		$this->SqlOp->close();
+		$this -> SqlOp -> connectTo();
+		$result = $this -> SqlOp -> updateItem("User", $setKeys, $setValues, $searchKey, $searchValue);
+		$this -> SqlOp -> close();
 		return $result;
-	}	
+	}
 
 	public function selectUserWithId($id) {
-		return $this->selectUserWithSearchKeyAndValue('id', $id);
+		return $this -> selectUserWithSearchKeyAndValue('id', $id);
 	}
 
 	public function selectUserWithAcount($account) {
-		return $this->selectUserWithSearchKeyAndValue('account', $account);
+		return $this -> selectUserWithSearchKeyAndValue('account', $account);
 	}
 
-    public function selectUserWithUsername($username) {
-		return $this->selectUserWithSearchKeyAndValue('username', $username);
+	public function selectUserWithUsername($username) {
+		return $this -> selectUserWithSearchKeyAndValue('username', $username);
 	}
 
 	public function deleteModel() {
-		$this->SqlOp->connectTo();
-		$result = $this->SqlOp->deleteItem("User", 'id',$this->id);
-		$this->SqlOp->close();
-		return $result;	
-	}
-
-//Private Method
-
-	private function selectUserWithSearchKeyAndValue($key, $value) {
-		$this->SqlOp->connectTo();
-		$result = $this->SqlOp->selectItem("User",$key,$value);
-		$this->SqlOp->close();
+		$this -> SqlOp -> connectTo();
+		$result = $this -> SqlOp -> deleteItem("User", 'id', $this -> id);
+		$this -> SqlOp -> close();
 		return $result;
 	}
+
+	//Private Method
+	private function selectUserWithSearchKeyAndValue($key, $value) {
+		$this -> SqlOp -> connectTo();
+		$result = $this -> SqlOp -> selectItem("User", $key, $value);
+		$this -> SqlOp -> close();
+		return $result;
+	}
+
+}
 ?>
